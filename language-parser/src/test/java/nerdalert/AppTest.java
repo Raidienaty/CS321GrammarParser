e9fcf358c69ac89211864b2d8a62421ec03a4f4d
TTTriplicate <tttriplicate@gmail.com>
2021-04-16 15:44:15 -0400

    Got the actual tests working.  Had to add somethings in the pom

--- language-parser/src/test/java/nerdalert/AppTest.java
+++ language-parser/src/test/java/nerdalert/AppTest.java
@@ -1,8 +1,8 @@
 package nerdalert;
 
 import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
@@ -13,6 +13,7 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
+    @DisplayName("Testing that testing runs")
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
