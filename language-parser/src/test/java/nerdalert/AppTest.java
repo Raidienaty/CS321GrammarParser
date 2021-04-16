161abb4f54bd89933ebc593ce981b3bf7d9a845d
TTTriplicate <tttriplicate@gmail.com>
2021-04-16 16:22:27 -0400

    Gonna need to 'borrow' some boilerplate for these tests

--- language-parser/src/test/java/nerdalert/AppTest.java
+++ language-parser/src/test/java/nerdalert/AppTest.java
@@ -1,14 +1,27 @@
 package nerdalert;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import static org.junit.Assert.assertTrue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.CharStreams;
+
+import parser.*;
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+
     /**
      * Rigorous Test :-)
      */
@@ -17,5 +30,18 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    @Test
+    public void setVariable() throws IOException
+    {
+        CharStream inputCharStream = CharStreams.fromFileName("language-parser/src/test/java/nerdalert/varTest.dcf");
+        Lexer lexer = new LanguageLexer(inputCharStream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        LanguageParser parser = new LanguageParser(tokens);
+        ParseTree parseTree = parser.start();
+    
+        EvalVisitor visitor = new EvalVisitor();
+    
+        assertTrue(visitor.visit(parseTree) == "1");
     }
 }
