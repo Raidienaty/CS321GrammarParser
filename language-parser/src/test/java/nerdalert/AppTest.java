9c492188df1e49011806bfae1a672b1943b77bac
TTTriplicate <tttriplicate@gmail.com>
2021-04-18 10:19:24 -0400

    Basic assignment tests done; need to pull in some updates to fully finish

--- language-parser/src/test/java/nerdalert/AppTest.java
+++ language-parser/src/test/java/nerdalert/AppTest.java
@@ -3,6 +3,8 @@ package nerdalert;
 import java.io.IOException;
 import java.io.InputStream;
 
+import java.util.List;
+
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,6 +16,7 @@ import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
 
 import parser.*;
 
@@ -24,24 +27,28 @@ import parser.*;
 public class AppTest 
 {
 
-    private CommonTokenStream getTokensFromText(String txt){
+    private List<Token> getTokensFromText(String txt){
         CharStream stream = CharStreams.fromString(txt);
         Lexer lexer = new LanguageLexer(stream);
 
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
         tokens.fill();
-        return tokens;    
+        return tokens.getTokens();    
     }
     /**
      * Rigorous Test :-)
      */
     @Test
-    @DisplayName("Basic Lexer Tests")
-    public void tokenTest() throws IOException
+    public void assignmentTest() throws IOException
     {
-        CommonTokenStream tokens = getTokensFromText("var = 1;");
+        List<Token> tokens = getTokensFromText("var = 1;");
 
         assertEquals(tokens.size(), 5);
+        assertEquals(LanguageLexer.VARIABLENAME, tokens.get(0).getType());
+        //assertEquals(LanguageLexer., tokens.get(0).getType());
+        assertEquals(LanguageLexer.NUMBER, tokens.get(2).getType());
+        assertEquals(LanguageLexer.SEMICOLON, tokens.get(3).getType());
+        assertEquals(LanguageLexer.EOF, tokens.get(4).getType());
     }
 }
